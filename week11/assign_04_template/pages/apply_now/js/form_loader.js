40102363b61e33d04382780d57c466708d6829ca
bashir <bashir.ahmedd@gmail.com>
2023-11-11 00:14:21 -0500

    form input template

---
+++ week11/assign_04_template/pages/apply_now/js/form_loader.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    console.log('form loader called');
+    // $("#home-nav").load('../../components/child-l1.html');
+    load_frag('apply_for_international.html');
+});
+
+function onclick_loadform(event) {
+    event.preventDefault();
+    const form_id = $(event.target).attr("id");
+    console.log(form_id);
+    const fname =  form_id + '.html';
+    debugger;
+    load_frag(fname)
+}
+
+function load_frag(filename){
+    $("#form-container").load(filename, function (response, status) {
+        if (status === 'error') {
+            console.log("Failed to load menu.html");
+        }
+        else {
+            console.log("Success!");
+        }
+    });
+}
+
+function luckyone(){
+    alert("You are lucky to reach this far....");
+}
