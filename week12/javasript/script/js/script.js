2679b194d58bd972fae5d40959ccff7ac317360d
bashir <bashir.ahmedd@gmail.com>
2023-11-26 00:43:40 -0500

    week 13

--- week12/javasript/script/js/script.js
+++ week12/javasript/script/js/script.js
@@ -1,11 +1,16 @@
 function createParagraph() {
-    const para = document.createElement('p');
-    para.textContent = 'You clicked the button!';
-    document.body.appendChild(para);
-  }
-  
-  const buttons = document.querySelectorAll('button');
+  const para = document.createElement('p');
+  para.textContent = 'You clicked the button!';
+  document.body.appendChild(para);
+}
+
+const button = document.getElementById("btnClick");
+button.addEventListener('click', createParagraph);
+
+/* 
+  const buttons = document.querySelectorAll('button'); 
+ const buttons = document.querySelectorAll('button');
   
   for (const button of buttons) {
     button.addEventListener('click', createParagraph);
-  }
+  } */
